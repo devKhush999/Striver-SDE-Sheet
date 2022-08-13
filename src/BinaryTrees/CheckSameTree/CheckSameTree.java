74879bc39200e3952614c203ebcd8b47ad49f734
devKhush999 <khushdev7838@gmail.com>
2022-08-13 18:29:29 +0530

    Check Two trees are same

---
+++ src/BinaryTrees/CheckSameTree/CheckSameTree.java
@@ -0,0 +1,53 @@
+package BinaryTrees.CheckSameTree;
+
+// https://youtu.be/BhuvF_-PWS0
+// https://takeuforward.org/data-structure/check-if-two-trees-are-identical/
+// https://www.geeksforgeeks.org/write-c-code-to-determine-if-two-trees-are-identical/
+
+class CheckSameTree {
+    /************************************ Efficient Solution *****************************************
+     * Simple Pre-Order Traversal
+     * Approach: In order to check whether two trees are identical or not, we need to traverse the
+        trees.
+     * While traversing, we first check the value of the nodes, if they are unequal we can simply
+        return false, as trees are non-identical.
+        If they are the same, then we need to recursively check their left child as well as the right child.
+     * When we get all the three values as true (node values, left child equals, right child equals)
+        we can conclude that these are identical trees and can return true.
+        Any other combination will return false.
+
+     * Time Complexity: O(n)
+         * We are doing a Single Pre-Order tree traversal
+     * Space Complexity: O(n)
+        * Space is needed for the recursion stack.
+        * In the worst case (skewed tree), space complexity can be O(N).
+     */
+    // Technique Used: Pre-Order Traversal
+    /* Given two trees, return true if they are structurally identical */
+    public boolean isSameTree(TreeNode A, TreeNode B) {
+        // Both Trees are Empty, return true
+        if (A == null && B == null)
+            return true;
+
+        // If any of them is non-empty, then they are not equal
+        if (A == null || B == null)
+            return false;
+
+        // Check data of the root nodes (tree1->data ==  tree2->data)
+        if (A.val != B.val)
+            return false;
+
+        // Check Left SubTrees & Right SubTrees recursively
+        // i.e., call sameTree(tree1->left_subtree, tree2->left_subtree)
+        // and call sameTree(tree1->right_subtree, tree2->right_subtree)
+        return isSameTree(A.left, B.left)  &&  isSameTree(A.right, B.right);
+    }
+
+
+    // Tree Node class
+    private static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) { this.val = val; }
+    }
+}
