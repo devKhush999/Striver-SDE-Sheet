881538c56a5e56dd855cb4b94d58a2f065652927
devKhush999 <khushdev7838@gmail.com>
2022-08-15 16:06:33 +0530

    Minor edit

--- src/BinaryTrees/CheckSameTree/CheckSameTree.java
+++ src/BinaryTrees/CheckSameTree/CheckSameTree.java
@@ -1,4 +1,6 @@
 package BinaryTrees.CheckSameTree;
+import java.util.LinkedList;
+import java.util.Queue;
 
 // https://youtu.be/BhuvF_-PWS0
 // https://takeuforward.org/data-structure/check-if-two-trees-are-identical/
@@ -43,6 +45,49 @@ class CheckSameTree {
         return isSameTree(A.left, B.left)  &&  isSameTree(A.right, B.right);
     }
 
+
+    /************************************ BFS Solution **********************************************
+     * Approach: Idea is to use a BFS
+
+     * Time Complexity: O(n)
+     * We will traverse all the Nodes only Once
+     * Space Complexity: O(Tree's Height)
+     * BFS Queue used
+     */
+    public boolean checkSameTree_BFS(TreeNode rootA, TreeNode rootB) {
+        // BFS Queue
+        // We can't use ArrayDeque() here, because it doesn't allow the null values to be added.
+        // There can be null values in Queue. So, use a LinkedList here
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(rootA);
+        queue.add(rootB);
+
+        while (!queue.isEmpty()){
+            // We will pop out 2 nodes from the Queue at a time
+            TreeNode A = queue.remove();
+            TreeNode B = queue.remove();
+
+            if (A == null && B == null)     // Same logic used in DFS
+                continue;
+
+            if (A == null || B == null)     // Same logic used in DFS
+                return false;
+
+            if (A.val != B.val)             // Same logic used in DFS
+                return false;
+
+            // A.left and B.left should be equal in structure, in order for Tree to be equal
+            // So, we have to add A.left & B.left in Queue Together so that they can be popped out together
+            queue.add(A.left);
+            queue.add(B.left);
+
+            // A.right and B.right should be equal in structure, in order for Tree to be equal
+            // So, we have to add A.right & B.right in Queue Together so that they can be popped out together
+            queue.add(A.right);
+            queue.add(B.right);
+        }
+        return true;
+    }
 
     // Tree Node class
     private static class TreeNode {
